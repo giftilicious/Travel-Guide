06f11ebad693be5e33fc221a4c3d0c4079e47593
Romal Patel <romzz.patel@gmail.com>
2021-10-01 23:13:30 -0400

    Updated weather icons

--- assets/script/resultsScript.js
+++ assets/script/resultsScript.js
@@ -72,22 +72,22 @@ function weatherApiFunc() {
 
             var wclearsky = document.createElement('img');
             wclearsky.setAttribute('src', "assets/images/clearsky.png");
-            wclearsky.setAttribute('style', "max-width:10% ");
+            wclearsky.setAttribute('style', "max-width:20% ");
             var wDrizzle = document.createElement('img');
             wDrizzle.setAttribute('src', "assets/images/drizzle.png");
-            wDrizzle.setAttribute('style', "max-width:10% ");
+            wDrizzle.setAttribute('style', "max-width:20% ");
             var wFoggy = document.createElement('img');
             wFoggy.setAttribute('src', "assets/images/foggy.png");
-            wFoggy.setAttribute('style', "max-width:10% ");
+            wFoggy.setAttribute('style', "max-width:20% ");
             var wHeavyrain = document.createElement('img');
             wHeavyrain.setAttribute('src', "assets/images/heavyrain.png");
-            wHeavyrain.setAttribute('style', "max-width:10% ");
+            wHeavyrain.setAttribute('style', "max-width:20% ");
             var wOvercast = document.createElement('img');
             wOvercast.setAttribute('src', "assets/images/overcast.png");
-            wOvercast.setAttribute('style', "max-width:10% ");
+            wOvercast.setAttribute('style', "max-width:20% ");
             var wPartlycloudy = document.createElement('img');
             wPartlycloudy.setAttribute('src', "assets/images/partlycloudy.png");
-            wPartlycloudy.setAttribute('style', "max-width:10% ");
+            wPartlycloudy.setAttribute('style', "max-width:20% ");
 
             if (data.current_weather.weathercode === 0) {
                 dynaWeather.append(wclearsky);
