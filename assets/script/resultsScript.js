3a7498e89ab0f9f3d026c915735f394f78e87769
romzz.git <romzz.patel@gmail.com>
2021-10-04 02:02:11 -0400

    added comments on the javascript file

--- assets/script/resultsScript.js
+++ assets/script/resultsScript.js
@@ -1,3 +1,4 @@
+//selects the url
 let queryString = document.location.search;
 
 let latitudeArray1 = queryString.split('=')[1];
@@ -7,10 +8,12 @@ let latitude = latString.split('&')[0];
 console.log(latitude,
     longitude);
 
+//runs the function on page load
 window.onload = function () {
     trailApi(), weatherApiFunc();
 };
 
+//runs the api showing the information about the trail location by taking the latitude and longitude from the first api
 function trailApi() {
     fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=" + latitude + "&lon=" + longitude + "&per_page=20", {
         "method": "GET",
@@ -24,11 +27,13 @@ function trailApi() {
         })
         .then(function (data) {
             console.log(data)
+            //replaces the defined image if the image from the api is not available
             data.data.forEach(diffi => {
                 if (diffi.thumbnail == null) {
                     diffi.thumbnail = "https://images.unsplash.com/photo-1600284536251-8bb98db53468?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dHJhaWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
                 }
 
+                // creates the cards in the HTML from the available information from the API
                 document.getElementById('divContainer').insertAdjacentHTML("beforeend",
                     `<div class="bg-white rounded-lg border border-gray-200 shadow-md">
                     <!-- image -->
@@ -54,6 +59,8 @@ function trailApi() {
             });
         });
 }
+
+//run the weather API function, it takes the latitude and longitude fron the first API and gives the weather info for the given location
 function weatherApiFunc() {
     var weatherUrl = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=sunrise,sunset,winddirection_10m_dominant&current_weather=true&timezone=America%2FNew_York";
     fetch(weatherUrl)
@@ -70,6 +77,7 @@ function weatherApiFunc() {
             var trailWeathercode = data.current_weather.weathercode;
             var trailWindspeed = "   Current Windspeed: " + data.current_weather.windspeed + " Km/hr";
 
+            //creates the weather icon variable for given weather type
             var wclearsky = document.createElement('img');
             wclearsky.setAttribute('src', "assets/images/clearsky.png");
             wclearsky.setAttribute('style', "max-width:20% ");
@@ -89,6 +97,8 @@ function weatherApiFunc() {
             wPartlycloudy.setAttribute('src', "assets/images/partlycloudy.png");
             wPartlycloudy.setAttribute('style', "max-width:20% ");
 
+
+            //creates the weather icon for each weather code given by the API
             if (data.current_weather.weathercode === 0) {
                 dynaWeather.append(wclearsky);
             } else if (data.current_weather.weathercode === 1 || data.current_weather.weathercode === 2) {
@@ -103,6 +113,8 @@ function weatherApiFunc() {
                 dynaWeather.append(wHeavyrain)
             }
 
+
+            //shows the weather info on the results page
             var headerTemp = document.createElement('div');
             headerTemp.textContent = trailTemperature;
             headerTemp.style.color = "#0f65b1";
